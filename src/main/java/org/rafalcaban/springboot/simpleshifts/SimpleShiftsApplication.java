cc1f67e6dcad53110e37ba39c6a74323a9045f10
Rafał Caban <rcaban14@gmail.com>
2026-02-16 13:01:13 +0100

    Initial commit - spring boot setup

---
+++ src/main/java/org/rafalcaban/springboot/simpleshifts/SimpleShiftsApplication.java
@@ -0,0 +1,13 @@
+package org.rafalcaban.springboot.simpleshifts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleShiftsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleShiftsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rafalcaban/springboot/simpleshifts/SimpleShiftsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rafalcaban.springboot.simpleshifts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleShiftsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
