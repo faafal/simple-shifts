34b6f6667971ea9a1ef6c79e55a2223ec9c9c06a
faafal <rcaban14@gmail.com>
2026-02-16 20:57:20 +0100

    Add shift repository class

---
+++ src/main/java/org/rafalcaban/springboot/shift/ShiftRepository.java
@@ -0,0 +1,55 @@
+package org.rafalcaban.springboot.shift;
+
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Repository
+public class ShiftRepository {
+
+    private static final List<Shift> shifts = new ArrayList<>();
+    private static int counter = 0;
+
+    {
+        shifts.add(new Shift(
+                counter++,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusHours(8L),
+                2
+        ));
+        shifts.add(new Shift(
+                counter++,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusHours(8L),
+                2
+        ));
+        shifts.add(new Shift(
+                counter++,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusHours(8L),
+                3
+        ));
+    }
+
+    public Shift addShift(Shift shift) {
+        shift.setId(counter++);
+        shifts.add(shift);
+        return shift;
+    }
+
+    public List<Shift> getAllShifts(){
+        return Collections.unmodifiableList(shifts);
+    }
+
+    public boolean deleteShift(long shiftId){
+        return shifts.stream()
+                .filter(s -> s.getId() == shiftId)
+                .findFirst()
+                .map(shifts::remove)
+                .orElse(false);
+    }
+
+}
