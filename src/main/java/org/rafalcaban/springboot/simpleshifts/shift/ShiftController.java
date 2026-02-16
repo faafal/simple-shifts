7127d2635a20938467e5929d91495bfa90af0f3a
faafal <rcaban14@gmail.com>
2026-02-17 00:24:36 +0100

    Add shift controller

---
+++ src/main/java/org/rafalcaban/springboot/simpleshifts/shift/ShiftController.java
@@ -0,0 +1,31 @@
+package org.rafalcaban.springboot.simpleshifts.shift;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/shifts")
+public class ShiftController {
+
+    private final ShiftService service;
+
+    public ShiftController(ShiftService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Shift addShift(@RequestBody Shift shift) {
+        return service.addShift(shift);
+    }
+
+    @GetMapping
+    public List<Shift> getAllShifts() {
+        return service.getAllShifts();
+    }
+
+    @DeleteMapping("/{shiftId}")
+    public boolean deleteShift(@PathVariable long shiftId) {
+        return service.deleteShift(shiftId);
+    }
+}
