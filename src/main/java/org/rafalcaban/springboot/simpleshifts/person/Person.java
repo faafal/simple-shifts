4c769ce97ad08c79c2cc6eb9864e4fda7c5dc9be
faafal <rcaban14@gmail.com>
2026-03-08 12:03:15 +0100

    Add field validation

--- src/main/java/org/rafalcaban/springboot/simpleshifts/person/Person.java
+++ src/main/java/org/rafalcaban/springboot/simpleshifts/person/Person.java
@@ -1,9 +1,6 @@
 package org.rafalcaban.springboot.simpleshifts.person;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Person {
@@ -11,7 +8,9 @@ public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, length = 50)
     private String name;
+    @Column(nullable = false, length = 50)
     private String surname;
 
     public Person() {
